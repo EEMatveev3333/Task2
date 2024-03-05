8bddcdf45682f792a9301a4560366853d8000c2e
EEMatveev <eematveev333@gmail.com>
2024-03-05 07:51:31 +0300

    Commit9

--- Task2Prj/src/main/java/org/example/FractionCache.java
+++ Task2Prj/src/main/java/org/example/FractionCache.java
@@ -3,19 +3,26 @@ package org.example;
 public class FractionCache implements Fractionable{
     private Fraction fraction;
 
+    boolean isChanged;
+
+    double tmp;
+
     public FractionCache(Fraction fraction) {
         this.fraction = fraction;
     }
 
     public double doubleValue(){
-        return fraction.doubleValue();
+        if (isChanged) tmp = fraction.doubleValue();
+        return tmp;
     };
 
     public void setNum(int num){
         fraction.setNum(num);
+        isChanged = true;
     };
 
     public void setDenum(int denum){
         fraction.setDenum(denum);
+        isChanged = true;
     };
 }
--- Task2Prj/src/main/java/org/example/Utils.java
+++ Task2Prj/src/main/java/org/example/Utils.java
@@ -5,6 +5,7 @@ public class Utils {
 //    static <T> void add(List<T> list, T t) {
 //        list.add(t);
 //    }
+//    public T member1;
 
     static <T> T cache(T t) {
         //list.add(t);
