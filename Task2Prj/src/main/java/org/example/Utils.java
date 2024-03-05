37ffb632fc1a69d2f00699fb470e44e6383f07f1
EEMatveev <eematveev333@gmail.com>
2024-03-05 07:26:09 +0300

    Commit8

---
+++ Task2Prj/src/main/java/org/example/Utils.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Utils {
+
+//    static <T> void add(List<T> list, T t) {
+//        list.add(t);
+//    }
+
+    static <T> T cache(T t) {
+        //list.add(t);
+        return t;
+    }
+
+}
