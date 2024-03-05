1690b364ef2f320337bc9cd88920bdf7d0880e6c
EEMatveev <eematveev333@gmail.com>
2024-03-05 22:42:36 +0300

    Commit15 Task2

--- Task2Prj/src/main/java/org/example/Main.java
+++ Task2Prj/src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-
-
 //import java.util.*;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
@@ -19,14 +17,6 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
-//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-//        }
 
         Fraction fr= new Fraction(2,3);
         //--- Тестовый пример
