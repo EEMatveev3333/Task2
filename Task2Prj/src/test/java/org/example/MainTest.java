c51e6eebcd9026fd38cb02801e51a4678b879436
EEMatveev <eematveev333@gmail.com>
2024-03-19 13:16:39 +0300

    Commit23 Task2 With Full Tests

--- Task2Prj/src/test/java/org/example/MainTest.java
+++ Task2Prj/src/test/java/org/example/MainTest.java
@@ -5,61 +5,25 @@ import org.junit.jupiter.api.*;
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Тест 1.1 . Проверить корректность значений")
-    void testCorrectValue() {
-        Fraction fr = new Fraction(2,3);
-        System.out.println("===!");
-        Fractionable num = Utils.cache(fr);
-        assertEquals(String.valueOf(num.doubleValue()), String.valueOf(2.0/3.0));
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        num.setNum(5);
-        assertEquals(String.valueOf(num.doubleValue()), String.valueOf(5.0/3.0));
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        System.out.println("===!");
-    }
-
-    @org.junit.jupiter.api.Test
-    @DisplayName("Тест 1.2 . Проверить не корректность значений")
-    void testUnCorrectValue() {
-        Fraction fr = new Fraction(2,3);
-        System.out.println("===!");
-        Fractionable num = Utils.cache(fr);
-        assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(3.0/3.0));
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        num.setNum(5);
-        assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(7.0/3.0));
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
-        System.out.println("===!");
-    }
-    @org.junit.jupiter.api.Test
-    @DisplayName("Тест 2. Проверить своевременный пересчет значений")
+    @DisplayName("Тест 1. Проверить запись в кэш корректного значения")
     void testUnCachedValue() {
         Fraction fr = new Fraction(2,3);
         System.out.println("===!");
         Fractionable num = Utils.cache(fr);
-        assertEquals(String.valueOf(num.doubleValue()), String.valueOf(2.0/3.0));
+
+
         assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(4.0/3.0));
         num.setNum(5);
         assertEquals(String.valueOf(num.doubleValue()), String.valueOf(5.0/3.0));
         assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(6.0/3.0));
         num.setDenum(15);
-        assertEquals(String.valueOf(num.doubleValue()), String.valueOf(5.0/15.0));
-        assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(5.0/16.0));
-        assertNotEquals(String.valueOf(num.doubleValue()), String.valueOf(100.0/100.0));
-
         System.out.println("            " + String.valueOf(num.doubleValue()));// sout сработал
-        System.out.println("            " + String.valueOf(num.doubleValue()));// sout молчит
+        assertEquals(String.valueOf(Utils.referPersonInvocationHandler.ObjectsCache.get("doubleValue")), String.valueOf(5.0/15.0));
         System.out.println("===!");
     }
 
     @org.junit.jupiter.api.Test
-    @DisplayName("Тест 3. Проверить искуственно скорректированное кэшированное значение, на предмет чтения из кэша")
+    @DisplayName("Тест 2. Проверить чтение из кэша искуственно скорректированным кэшированное значением.")
     void testCrushedCachedValue() {
         Fraction fr = new Fraction(2,3);
         System.out.println("===!");
